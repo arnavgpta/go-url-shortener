1773b80048112992199a1ee8c68cf028bc3055db
Arnav gupta <garnav02@gmail.com>
2025-06-03 11:27:02 +0530

    Step 2: Set up basic HTTP server

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to the URL Shortener!")
+}
 
 func main() {
-	fmt.Println("Url shortener starting...")
+	http.HandleFunc("/", homeHandler)
+
+	fmt.Println("Server is running on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
